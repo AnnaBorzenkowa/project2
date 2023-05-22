a12f4e231e54d771452b4aa5a24ee7c8c6e28d84
anna borzenkowa <crownedjocker@gmail.com>
2023-05-22 16:57:09 +0300

    corrected java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         int rubles = 50;
-        int x = 100;
-        if (x >= 1000) {
-            System.out.println(((x + rubles) + (x / 100)) + "рублей на счете");
+        int deposite = 100;
+        if (deposite >= 1000) {
+            System.out.println(((deposite + rubles) + (deposite / 100)) + "рублей на счете");
         } else {
-            System.out.println((x + rubles) + "рублей на счете");
+            System.out.println((deposite + rubles) + "рублей на счете");
 
         }
     }
